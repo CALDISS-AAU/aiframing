f5cbea2b9d5c74b36e1c4acad95f7a13ca349527
Kristian Gade Kjelmann <kgk@adm.aau.dk>
2019-12-06 16:23:44 +0100

    added common bigram extraction

--- preprocessing/json_to_ngrams.R
+++ preprocessing/json_to_ngrams.R
@@ -19,37 +19,40 @@ filepath_mckin <- paste0(data_path, "mckin_articles.json")
 filepath_ey <- paste0(data_path, "ey_articles.json")
 filepath_bcg <- paste0(data_path, "bcg_articles.json")
 
-mckin_texts <- fromJSON(filepath_mckin)
-ey_texts <- fromJSON(filepath_ey)
-bcg_texts <- fromJSON(filepath_bcg)
+mckin_texts <- map(fromJSON(filepath_mckin), stripWhitespace)
+ey_texts <- map(fromJSON(filepath_ey), stripWhitespace)
+bcg_texts <- map(fromJSON(filepath_bcg), stripWhitespace)
 
-fill_text <- "our mission is to help leaders in multiple sectors develop a deeper understanding of the global economy. our flagship business publication has been defining and informing the senior-management agenda since 1964. our learning programs help organizations accelerate growth by unlocking their people's potential." 
-fill_text2 <- "for the full mckinsey global institute report upon which this article is based see  tech for good using technology to smooth disruption and improve wellbeing  jacques bughin is a director of the mckinsey global institute and a senior partner in mckinseys brussels office and eric hazan is a senior partner in the paris office please sign in to print or download this article please create a profile to print or download this article create a profile to get full access to our articles and reports including those by mckinsey quarterly and the mckinsey global institute and to subscribe to our newsletters and email alerts mckinsey uses cookies to improve site functionality provide you with a better browsing experience and to enable our partners to advertise to you detailed information on the use of cookies on this site and how you can decline them is provided in our cookie policy by using this site or clicking on ok you consent to the use of cookies sign up for email alerts select topics and stay current with our latest insights"
-fill_text3 <- "please sign in to print or download this article please create a profile to print or download this article create a profile to get full access to our articles and reports including those by mckinsey quarterly and the mckinsey global institute and to subscribe to our newsletters and email alerts mckinsey uses cookies to improve site functionality provide you with a better browsing experience and to enable our partners to advertise to you detailed information on the use of cookies on this site and how you can decline them is provided in our cookie policy by using this site or clicking on ok you consent to the use of cookies sign up for email alerts select topics and stay current with our latest insights"
 
 ##TEXT CLEANUP##
-text_cleanup <- function(text) {
-  text_nofill = gsub(fill_text, "", text)
-  text_nows = stripWhitespace(text_nofill)
-  text_nonumb = removeNumbers(text_nows)
-  text_nopunct1 = gsub("”", "", text_nonumb)
-  text_nopunct2 = gsub("’", "", text_nopunct1)
-  text_nopunct3 = gsub("‘", "", text_nopunct2)
-  text_nopunct4 = gsub("„", "", text_nopunct3)
-  text_nopunct5 = gsub("“", "", text_nopunct4)
-  text_nopunct6 = gsub(",", "", text_nopunct5)
-  text_clean = gsub(fill_text2, "", text_nopunct6)
-  text_clean = gsub(fill_text3, "", text_clean)
-  return(text_clean)
+mckin_fill_regex <- list(mission = "Our mission is to help .* help clients in new and exciting ways",
+                         sign = "Please sign in to print .* our newsletters and email alerts",
+                         cookies = "McKinsey uses cookies .* stay current with our latest insights",
+                         odds = "Improving your odds of success for large scale change .* your people to accelerate and sustain the change")
+
+ey_fill_regex <- list(read = "(\\d{1,2} minute .*? \\d{4})",
+                      about = "About this article .*? EY Global .*?",
+                      email = "(?<=\\.) Contact us .*? Read more\\s$",
+                      related = "Related topics .*? Link copied",
+                      link = "Link copied", 
+                      readmore = "Read more")
+
+punct_filt <- "”|’|‘|„|“|,"
+
+text_cleanup <- function(text, regex = list(), punct = "”|’|‘|„|“|,") {
+  for (regex in regex) {
+    text <- str_replace(text, regex, "")
+  }
+  text_nopunct = str_replace_all(text, punct, "")
+  return(text_nopunct)
 }
 
-
-mckin_texts_clean <- map(mckin_texts, text_cleanup)
-ey_texts_clean <- map(ey_texts, text_cleanup)
+mckin_texts_clean <- map(mckin_texts, text_cleanup, regex = mckin_fill_regex, punct = punct_filt)
+ey_texts_clean <- map(ey_texts, text_cleanup, regex = ey_fill_regex)
 bcg_texts_clean <- map(bcg_texts, text_cleanup)
 
 texts_to_df <- function(textlist) {
-  text_df = dplyr::as_data_frame((matrix(textlist))) %>%
+  text_df = as_tibble(matrix(textlist)) %>%
     mutate(title = names(textlist),
            text = unlist(V1)) %>%
     select(title, text)
@@ -63,6 +66,82 @@ ey_df <- texts_to_df(ey_texts_clean) %>%
 bcg_df <- texts_to_df(bcg_texts_clean) %>%
   mutate(agency = "Boston Consulting Group")
 
+## COMBINED DF
+cons_df <- union(mckin_df, ey_df) %>%
+  union(bcg_df)
+
+
+## FIND HYPPIGSTE BIGRAMS
+stop_custom <- c("")
+act_stop <- c(stopwords("english"), stop_custom)
+
+text_preproces <- function(text, stopvec){
+  text_nopunct = removePunctuation(text)
+  text_lower = str_to_lower(text_nopunct)
+  text_nonumb = removeNumbers(text_lower)
+  text_nostop = removeWords(text_nonumb, stopvec)
+  text_lenfilt = str_replace_all(text_nostop, "\\b[[:lower:]]{1,2}(\\s|$)|\\b[[:upper:]]{1}(\\s|$)", "")
+  return(text_lenfilt)
+}
+
+cons_pr_df <- cons_df %>%
+  mutate(text = text_preproces(text, act_stop))
+
+cons_bigram <- cons_pr_df %>%
+  unnest_tokens(bigram, text, token = "ngrams", n = 2) %>%
+  group_by(bigram) %>%
+  summarize(count = n())
+
+top_bi <- cons_bigram %>%
+  arrange(desc(count)) %>%
+  top_n(50)
+top_bi
+
+
+## TF-IDF
+cons_bigram_tfidf <- cons_pr_df %>%
+  unnest_tokens(bigram, text, token = "ngrams", n = 2) %>%
+  count(title, bigram, sort = TRUE) %>%
+  ungroup()
+
+total_bigrams <- cons_bigram_tfidf %>%
+  group_by(title) %>%
+  summarize(total = sum(n))
+
+cons_bigram_tfidf <- left_join(cons_bigram_tfidf, total_bigrams) %>%
+  bind_tf_idf(bigram, title, n)
+
+top_bigrams_tfidf <- cons_bigram_tfidf %>%
+  group_by(bigram) %>%
+  summarize(mean_tfidf = sum(tf_idf)) %>%
+  arrange(mean_tfidf) %>%
+  top_frac(0.0015, mean_tfidf)
+  
+
+# ANNOTATE
+setwd(work_path)
+#udmodel <- udpipe_download_model(language = "english")
+udmodel_eng <- udpipe_load_model(file = paste0(work_path, "english-ewt-ud-2.4-190531.udpipe"))
+
+bi_annotate <- function(bigram) {
+  anno_df = data.frame(udpipe_annotate(udmodel_eng, bigram))
+  pos = anno_df$upos
+  return(pos)
+}
+
+cons_bi_anno <- top_bigrams_tfidf %>%
+  mutate(word1 = str_extract(bigram, "^.*(?=\\s)"),
+         word2 = str_extract(bigram, "(?<=\\s).*$"),
+         pos = map(bigram, bi_annotate)) %>%
+  unnest_wider(col = pos) %>%
+  mutate(word1pos = ...1,
+         word2pos = ...2) %>%
+  select(everything(), -...1, -...2) %>%
+  filter(word1pos == "NOUN" | word2pos == "NOUN")
+         
+
+## NGRAMS
+
 mckin_ngram <- mckin_df %>%
   unnest_tokens(sentence, text, token = stringr::str_split, pattern = "\\.", to_lower = FALSE) %>%
   unnest_tokens(sentence, sentence, token = stringr::str_split, pattern = "\\?", to_lower = FALSE) %>%
@@ -71,37 +150,6 @@ mckin_ngram <- mckin_df %>%
   mutate(sent_num = 1:nrow(.)) %>%
   mutate(title = paste0(title, " - ", as.character(sent_num))) %>%
   unnest_tokens(ngrams, sentence, token = "ngrams", n = 10, to_lower = FALSE)
-  
-
-## FIND HYPPIGSTE BIGRAMS
-stop_custom <- c("")
-stopwords_act <- c(stopwords("english"), stop_custom)
-
-text_cleanup_nostop <- function(text, stopvec){
-  text_nofill = gsub(fill_text, "", text)
-  text_nopunct = removePunctuation(text_nofill)
-  text_nows = stripWhitespace(text_nopunct)
-  text_nostop = removeWords(text_nows, stopvec)
-  text_nopunct1 = gsub("”", "", text_nostop)
-  text_nopunct2 = gsub("’", "", text_nopunct1)
-  text_nopunct3 = gsub("‘", "", text_nopunct2)
-  text_nopunct4 = gsub("„", "", text_nopunct3)
-  text_nopunct5 = gsub("“", "", text_nopunct4)
-  text_clean = gsub(fill_text, "", text_nopunct5)
-  text_clean = gsub(fill_text2, "", text_clean)
-  text_clean = gsub(fill_text3, "", text_clean)
-  text_lenfilt = gsub("\\b\\w{1,2}\\s", "",text_clean)
-  return(text_lenfilt)
-}
-
-mckin_texts_nostop <- map(mckin_texts, text_cleanup_nostop, stopwords_act)
-mckin_nostop_df <- texts_to_df(mckin_texts_nostop) %>%
-  mutate(agency = "McKinsey")
-
-mckin_bigram <- mckin_nostop_df %>%
-  unnest_tokens(bigram, text, token = "ngrams", n = 2) %>%
-  group_by(bigram) %>%
-  summarize(count = n())
 
 ## UDLED NØGLEORD - TF-IDF?
 
